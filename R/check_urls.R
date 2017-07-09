f20a6ef98e3a78fc11101ba41be95eb8765c726d
Colin Gillespie <csgillespie@gmail.com>
2017-07-09 19:41:54 +0200

    Store bad URLs

--- R/check_urls.R
+++ R/check_urls.R
@@ -10,7 +10,8 @@ bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = 
 ## http://stackoverflow.com/q/26496538/203420
 url_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
-files = list.files("_book/", pattern = "*.html", full.names = TRUE)
+files = list.files("_book/", pattern = "*.html", full.names = TRUE)[9]
+bad_urls = c() # I know I'm growing a vector, but life is too short
 for(fname in files) {
   message(fname)
   urls = str_extract(readLines(fname), url_pattern)
@@ -20,6 +21,7 @@ for(fname in files) {
     test_url = httr::GET(url)
     if(test_url$status_code != 200) {
       message("\t", url, " ", test_url$status_code)
+      bad_urls = c(bad_urls, url)
     }
   }
 }
