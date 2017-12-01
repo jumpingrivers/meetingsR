da5b8e3408515248318b336f5a4ecd93bff2b666
Colin Gillespie <csgillespie@gmail.com>
2017-12-01 13:10:26 +0000

    Tweak deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,22 +8,21 @@ update_website() {
 
   git config --global user.name "jumpingrivers"
   git config --global user.email "csgillespie+jumpingrivers@gmail.com"
+  git remote add upstream "https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
 
-  git clone  \
-  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
-  book-output
-
-  cd book-output
-  cp -fvr ../_book/* docs/
-
-  cd docs
+  cp -fvr $BASE_REPO/_book/* .
   git add libs/; git add css/
   git add *.json; git add *.html; git add main.md; git add style.css
-  cd ..
-  git commit -a -m "Travis: updating website (${TRAVIS_BUILD_NUMBER})"
+  git commit -a -m "Updating book (${TRAVIS_BUILD_NUMBER})"
+  git status
   git push origin master 2>err.txt
   cd ..
+
+
+
+
 }
 
 update_website
-
