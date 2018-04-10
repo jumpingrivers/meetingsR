b07688a01fa1620dcadab108bdd8c960e3103b72
Colin Gillespie <csgillespie@gmail.com>
2018-04-10 21:18:20 +0100

    Bug in deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@ set -o errexit -o nounset
 BASE_REPO=$PWD
 
 update_website() {
+  Rscript -e 'source("R/parse_groups.R")'
+  Rscript -e 'source("R/parse_events.R")'
+
   cd ..; mkdir gh-pages; cd gh-pages
   git init
 
@@ -11,8 +14,6 @@ update_website() {
   git config --global push.default simple
   git remote add upstream "https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git"
   git fetch upstream 2>err.txt
-  Rscript -e 'source("R/parse_groups.R")'
-  Rscript -e 'source("R/parse_events.R")'
   git checkout gh-pages
 
   cp -fvr $BASE_REPO/_book/* .
