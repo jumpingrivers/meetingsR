b808c1b9680c33d3ddb43bebff3321df350510f0
Colin Gillespie <csgillespie@gmail.com>
2019-01-15 14:14:14 +0000

    Minor tweak to R script

--- R/check_urls.R
+++ R/check_urls.R
@@ -1,6 +1,5 @@
 ## Simple script for checking that URLs exist.
 ## What I really want to do is parse the .md file directly.
-
 library("stringr")
 ## Render book
 bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = FALSE)
@@ -10,7 +9,7 @@ bookdown::render_book("index.Rmd", output_format = "bookdown::gitbook", clean = 
 ## http://stackoverflow.com/q/26496538/203420
 url_pattern = "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
 
-files = list.files("_book/", pattern = "*.html", full.names = TRUE)[9]
+files = list.files("_book/", pattern = "*.html", full.names = TRUE)
 bad_urls = c() # I know I'm growing a vector, but life is too short
 for(fname in files) {
   message(fname)
