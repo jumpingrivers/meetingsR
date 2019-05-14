40ad2d7bbd537a2c00657a6d7a8589f979ac49d3
Colin Gillespie <csgillespie@gmail.com>
2019-05-14 14:13:28 +0100

    Used the idea from @liao961120 to add edit buttons to each child documents.

---
+++ R/pre_chap.R
@@ -0,0 +1,17 @@
+url = 'https://github.com/jumpingrivers/meetingsR/blob/master/'
+gh_edit_path = paste0(url, knitr::current_input())
+#
+edit_btn = paste0('<a href="', gh_edit_path,
+                  '" class = "h2-side-link">',
+                  '<img src="https://bit.ly/2RRirG7" ',
+                  'alt="fa-edit" ',
+                  'class="edit"></a>')
+
+
+#url <- 'https://github.com/liao961120/parallelCode/edit/master/'
+#gh_edit_path <- paste0(url, knitr::current_input())
+
+# edit_btn <- paste0('<a href="', gh_edit_path, '">',
+#                    '<img src="https://bit.ly/2RRirG7" ',
+#                    'alt="fa-edit" ',
+#                    'class="edit"></a>')
