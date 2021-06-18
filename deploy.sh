1fb5129aace0546f3dc02396e60d06172e650e89
Colin Gillespie <csgillespie@gmail.com>
2021-06-18 13:55:15 +0100

    Update

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,15 @@ update_website() {
   git config --global user.name "jumpingrivers"
   git config --global user.email "csgillespie+jumpingrivers@gmail.com"
   git config --global push.default simple
+  echo "Remote 1"
   git remote add upstream "https://${GITHUB_PAT}@github.com/${GITHUB_REPOSITORY}.git"
+  echo "Remote 2"
   git fetch upstream 2>err.txt
+  echo "Remote 3"
   git checkout gh-pages
-
+  echo "Remote 4"
   cp -fvr ${GITHUB_WORKSPACE}/_book/* .
+  echo "Remote 5"
   git add libs/*; git add css/*
   git add *.json; git add *.html; git add main.md; git add style.css events.csv groups.csv virtual.csv
   git commit -a -m "#rstats ${COMMIT_MESSAGE} (${GITHUB_JOB})"
