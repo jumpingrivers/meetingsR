3938ba76a49db11efdd47a528201b6f6710113e1
Colin Gillespie <csgillespie@gmail.com>
2022-01-04 08:17:34 +0000

    Adding new style

---
+++ content/_index.md
@@ -0,0 +1,76 @@
+---
+title: "Jr MeetingsR"
+layout: "index"
+
+hero:
+    heading: "A list of R conferences and meetings"
+    text: "This site attempts to list R conferences and local useR groups. Please feel free to add any missing group or conference. In particular, most of the associated twitter names are missing. There are currently 466 R user groups and events. To propose a change, just click the pencil icon next to the titles.
+
+    
+    Keep up to date by following [@rstats_meetings](https://twitter.com/rstats_meetings).
+
+    
+    This list is maintained by Jumping Rivers. We also maintain a corresponding list of [Data Science conferences and events](#)."
+
+cards: 
+    -
+        imageUrl: "/images/events.png"
+        imageDescription: ""
+        heading: "EVENTS"
+        slug: "events"
+        text: "Lorem ipsum dolor sit amet, possit tibique no eam, porro decore eu sea"
+        buttonName: "Events"
+    -
+        imageUrl: "/images/r.png"
+        imageDescription: ""
+        heading: "R User Groups"
+        slug: "r-user-groups"
+        text: "Lorem ipsum dolor sit amet, possit tibique no eam, porro decore eu sea"
+        buttonName: "View R User Groups"
+           
+    -
+        imageUrl: "/images/r-ladies.png"
+        imageDescription: ""
+        heading: "R-Ladies Groups"
+        slug: "r-ladies-groups"
+        text: "Lorem ipsum dolor sit amet, possit tibique no eam, porro decore eu sea"
+        buttonName: "View R-Ladies Groups"
+            
+    -
+        imageUrl: "/images/virtual.png"
+        imageDescription: ""
+        heading: "Virtual Events"
+        slug: "virtual-events"
+        text: "Lorem ipsum dolor sit amet, possit tibique no eam, porro decore eu sea"
+        buttonName: "View Virtual Events"
+            
+         
+
+data:
+    heading: "THE DATA"
+    text: "All files used to build this site can be found on the associated GitHub page.
+    
+    
+    Additionally, you can download a convenient CSV file of the data"
+
+    items:
+        -
+            itemName: "Conferences:"
+            itemUrlName: "https://jumpingrivers.github.io/meetingsR/events.csv"
+            itemUrl: "https://jumpingrivers.github.io/meetingsR/events.csv"
+        -
+            itemName: "User Groups:"
+            itemUrlName: "https://jumpingrivers.github.io/meetingsR/groups.csv"
+            itemUrl: "https://jumpingrivers.github.io/meetingsR/groups.csv"
+        -
+            itemName: "Virtual events:"
+            itemUrlName: "https://jumpingrivers.github.io/meetingsR/virtual.csv"
+            itemUrl: "https://jumpingrivers.github.io/meetingsR/virtual.csv"
+
+    footer:
+        textOne: "The R scripts used to generate these CSV files are also located in the GitHub repo"
+        linksName: "https://github.com/jumpingrivers/meetingsR/tree/master/R"
+        linksUrl: "https://github.com/jumpingrivers/meetingsR/tree/master/R"
+        textTwo: "These scripts were initially created via the [eRum competition](#)"
+    
+---
---
+++ themes/jr-meetingsr/static/javascript/script.js
@@ -0,0 +1,82 @@
+window.onload= function (){
+    stickyFunctionality();
+}
+window.addEventListener('resize',e=>{
+    stickyFunctionality();
+})
+
+function stickyFunctionality(){
+        const header= document.querySelector('.card-details__header');
+        if(header){
+            const scrollTopIcon = document.querySelector('.scroll-top');
+            const sidebar= document.querySelector('.card-details__sidebar');
+            const mainHeader= document.querySelector('.header');
+            const mainContentWrapper= document.querySelector('.card-details__category-wrapper');
+            let headerMaxWidth= document.querySelector('.card-details__schedule').clientWidth;
+            let sidebarMaxWidth= sidebar.clientWidth;
+            const mainHeaderheight= mainHeader.offsetHeight+3;
+            const screenWidth= window.innerWidth;
+            let containerWidth= document.querySelector('.container').clientWidth;
+            const containerLeftPadding = getComputedStyle(document.querySelector('.container')).paddingLeft;
+            let leftSideWhiteSpace= ((screenWidth-containerWidth)/2) + parseFloat(containerLeftPadding);
+            
+            let computerScreen= window.matchMedia('(min-width:768px)');
+            let computerScreenMax= window.matchMedia('(max-width:767px)');
+
+            document.addEventListener('scroll',e=>{
+            
+                    if(window.scrollY>mainHeaderheight){
+                    
+                        if(computerScreen.matches){
+                            sidebar.style.cssText = `max-width: ${sidebarMaxWidth}px; left: ${headerMaxWidth+leftSideWhiteSpace+20}px` ;
+                        }
+                        
+                        header.style.cssText = `position: fixed; top: 0; z-index: 1000; max-width: ${headerMaxWidth}px;`;
+                        mainContentWrapper.style.paddingTop= `${header.offsetHeight}px`;
+                    
+                    }else{
+                        if(computerScreen.matches){
+                            sidebar.removeAttribute('style');
+                        }
+                    
+                        header.removeAttribute('style');
+                        mainContentWrapper.removeAttribute('style');
+                    }
+
+                    //scrollTop start here
+                    if(window.scrollY>(window.innerHeight*50/100)){
+                        if(computerScreenMax.matches){
+                            scrollTopIcon.style.display="grid";
+                        }
+                    }else{
+                        scrollTopIcon.removeAttribute("style");
+                    }
+            })
+        }  
+}
+
+
+//Header functionality
+const header = document.querySelector('.header__nav')
+const openIcon = document.querySelector('.header__nav-icon')
+const closeIcon = document.querySelector('.header__close-circle')
+const body = document.querySelector('body');
+let scrollyaxis;
+
+openIcon.addEventListener('click',e=>{
+    header.style.display = 'grid';
+    scrollyaxis= window.scrollY;
+    body.style.position=`fixed`;
+    body.style.top=`-${scrollyaxis}px`;
+    body.style.left=`0`;
+    body.style.right=`0`;
+})
+
+closeIcon.addEventListener('click',e=>{
+    header.style.display = 'none';
+    body.style.removeProperty('position')
+    body.style.removeProperty('top')
+    body.style.removeProperty('left')
+    body.style.removeProperty('right')
+    window.scrollTo(0, scrollyaxis);
+})
