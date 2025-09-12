e3425f22fdc48c4371bcc93f01a983edfeff8d57
Colin Gillespie <csgillespie@gmail.com>
2025-09-12 09:46:53 +0100

    chore: Use air formatting (#256)

--- R/count_groups.R
+++ R/count_groups.R
@@ -3,7 +3,7 @@
 ## Input: Rmd files names
 count_groups = function(fnames) {
   len = -2 # For two example sites
-  for(fname in fnames) {
+  for (fname in fnames) {
     x = paste(readLines(fname), collapse = "\n")
     len = len + length(gregexpr("\\*", x)[[1]])
   }
